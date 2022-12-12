ac39e6be40c3764209f51b9d08bc2bd4533a843b
Ahni Anderson <115118638+ahndawn@users.noreply.github.com>
2022-12-12 09:22:44 -0500

    add submit, favorite, and saved stories functions

--- js/nav.js
+++ js/nav.js
@@ -6,24 +6,66 @@
 
 /** Show main list of all stories when click site name */
 
-function navAllStories(evt) {
-  console.debug("navAllStories", evt);
-  hidePageComponents();
+function allStories(evt) {
+  console.debug("allStories", evt);
+  hidePageComp();
   putStoriesOnPage();
 }
 
-$body.on("click", "#nav-all", navAllStories);
+$body.on("click", "#nav-all", allStories);
+
+/** Show story submit form on clicking story "submit" */
+
+function submitStoryClick(evt) {
+  console.debug("submitStoryClick", evt);
+  hidePageComp();
+  $allStoriesList.show();
+  $submitForm.show();
+}
+
+$navSubmitStory.on("click", submitStoryClick);
+
+/** Show favorite stories on click on "favorites" */
+
+function favoritesClick(evt) {
+  console.debug("favoritesClick", evt);
+  hidePageComp();
+  putFavoritesListOnPage();
+}
+
+$body.on("click", "#nav-favorites", favoritesClick);
+
+/** Show My Stories on clicking "my stories" */
+
+function myStories(evt) {
+  console.debug("myStories", evt);
+  hidePageComp();
+  putUserStoriesOnPage();
+  $ownStories.show();
+}
+
+$body.on("click", "#nav-my-stories", myStories);
 
 /** Show login/signup on click on "login" */
 
-function navLoginClick(evt) {
-  console.debug("navLoginClick", evt);
-  hidePageComponents();
+function loginClick(evt) {
+  console.debug("loginClick", evt);
+  hidePageComp();
   $loginForm.show();
   $signupForm.show();
 }
 
-$navLogin.on("click", navLoginClick);
+$navLogin.on("click", loginClick);
+
+/** Hide everything but profile on click on "profile" */
+
+function profileClick(evt) {
+  console.debug("profileClick", evt);
+  hidePageComp();
+  $userProfile.show();
+}
+
+$navUserProfile.on("click", profileClick);
 
 /** When a user first logins in, update the navbar to reflect that. */
 
